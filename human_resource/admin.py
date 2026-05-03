b0e806a0fa7426de28c4383fba13bc959c3f5e65
ziko278 <odekeziko@gmail.com>
2026-05-03 13:32:07 +0100

    admin updated

--- human_resource/admin.py
+++ human_resource/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from human_resource.models import StaffProfileModel, StaffModel, StaffWalletModelz
+from human_resource.models import StaffProfileModel, StaffModel, StaffWalletModel
 
 
 admin.site.register(StaffProfileModel)
